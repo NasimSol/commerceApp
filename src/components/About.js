600b238f27b34131f3286b47c4d0c6b6cc7aed0d
NASIM SOLEYMANI DEHKORDI <nasim.soleymanid@gmail.com>
2022-03-29 14:31:50 +0430

    commit 1

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+        <h1>About Us</h1>
+        <p className='lead'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <Link to='/contact' className="btn btn-outline-dark">Contact Us</Link>
+        </div>
+        </div>
+        
+        </div>
+    </div>
   )
 }
 
