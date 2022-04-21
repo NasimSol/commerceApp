27ecda48eff8eaafd4b35b0c603377e5f773b33e
NASIM SOLEYMANI DEHKORDI <nasim.soleymanid@gmail.com>
2022-04-21 07:03:20 +0430

    add few massage in another branch

--- src/components/Login.js
+++ src/components/Login.js
@@ -33,7 +33,7 @@ function Login() {
                   <input  autofocus type="password" class="form-control" id="exampleInputPassword1" />
                 </div>
                 <div class="mb-3 form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                  <input autofocus type="checkbox" class="form-check-input" id="exampleCheck1" />
                   <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
                 <button type="submit" class="btn btn-outline-primary w-100">Submit</button>
