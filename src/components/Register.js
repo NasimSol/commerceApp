48d03b2dd87dc290bae11f6d0cd0fca7705bc5b2
NASIM SOLEYMANI DEHKORDI <nasim.soleymanid@gmail.com>
2022-05-08 14:57:00 +0430

    Use formik in Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,57 +1,51 @@
 import React, { useState } from 'react';
 import { useTranslation } from "react-i18next";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { ToastContainer, toast } from 'react-toastify';
 
 function Register() {
   const { t } = useTranslation();
-  const [entrance, setEntrance] = useState({
-    username: '',
-    password: '',
-    email: ''
-  });
-  const [errors, setErrors] = useState({});//variable for errors;
-  const [accuracy, setAccuracy] = useState(true);//for able checkbox
   const [butt, setButt] = useState(true);
-  
-  //simple way for validate check length of entrance each input and then them don't exist retun errros
-  //There are no restrictions
- const onCheckValue = (e) => setButt(!butt);
 
 
-  const validate = () => {
-    const err = {}
-    if (entrance.username === '') { err.username = t('errorusername') }
-    if (entrance.password === '') { err.password = t('errorpassword') }
-    if (entrance.email === '') { err.email = t('erroremail') }
-    
+//use formik in this component
+   const formik = useFormik({
+     initialValues: {
+       username : '',
+       password: '',
+       email: '',
+     },
 
-    return Object.keys(err) === 0 ?null  : err;
-    
-  }
-  
-  const handleSubmit = (e) => {
-    //e.preventDefault();
-    
-    
-    setErrors(validate());
+ validationSchema: Yup.object({
+       username: Yup.string()
+         .min(8,t('error write'))
+         .required(t('required')),
+       password: Yup.string()
+       .required(t('required'))
+        
+       .matches(
+      /^(?=.*[0-9])(?=.{8,}).*$/,t('errorregister')),
+   email: Yup.string().email(t('invalid email address')).required(t('required'))
    
-    if (errors) return;//if exist error don't submit it;
-    console.log('submitted');
-    
-    setAccuracy(false);
-    
-    
-  }
-   
+     }),
 
-  const handleChange = (e) => {
-    setEntrance({
-      ...entrance,
-      [e.target.name]: e.target.value
-    });
-    if (entrance.email !== '' && entrance.username !== '' && entrance.password.length ===8&&entrance.password!=='' ) {
-      setAccuracy(!accuracy)
+     onSubmit: (values) => {
+      
+    return toast.success(t('Success register!'), {
+        backgroundColor: 'red',
+        position: "top-right",
+        autoClose: 2000
+     
+      });
     }
-  };
+     
+   });
+  console.log(formik.errors)
+  const err = Object.keys(formik.errors).length;
+
+  
+ const onCheckValue = (e) => setButt(!butt);
 
   return (
     <>
@@ -71,33 +65,54 @@ function Register() {
               <button className='btn btn-primary mb-4 w-100'><i className="fa fa-google mx-1" ></i>{t('sign in with google')}</button>
               <button className='btn btn-primary mb-4 w-100'><i className="fa fa-twitter mx-1" ></i>{t('sign in with facebook')}</button>
               
-              <form>
+              <form onSubmit={formik.handleSubmit}>
                 <div className="mb-3">
                   <label htmlFor="exampleInputEmail1" className="form-label">{t('username')}</label>
-                  <input name='username' value={entrance.username} onChange={(e)=>handleChange(e)} autoFocus type="text" className="form-control" id="exampleInput" />
+                  <input name='username'
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    autoFocus type="text"
+                    className="form-control"
+                    id="exampleInput" />
                   
-                  {errors.username && <div class="alert alert-danger" role="alert">{errors.username}</div>}
+                  {formik.touched.username&&formik.errors.username? <div className="alert alert-danger" role="alert">{formik.errors.username}</div>:null}
                 </div>
                   <div className="mb-3">
                   <label htmlFor="exampleInputEmail1" className="form-label">{t('Email address')}</label>
-                  <input name='email' value={entrance.email} onChange={(e)=>handleChange(e)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                   {errors.email && <div class="alert alert-danger" role="alert">{errors.email}</div>}
+                  <input name='email'
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    type="email" className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp" />
+                   {formik.touched.email&& formik.errors.email ? <div className="alert alert-danger" role="alert">{formik.errors.email}</div>:null}
                   <div id="emailHelp" className="form-text">{t('warning')}</div>
                 </div>
                 <div className="mb-3">
                   <label htmlFor="exampleInputPassword1" className="form-label">{t('password')}</label>
-                  <input name='password' value={entrance.password} onChange={(e)=>handleChange(e)} type="password" className="form-control" id="exampleInputPassword1" />
-                  {errors.email && <div class="alert alert-danger" role="alert">{errors.email}</div>}
+                  <input name='password'
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1" />
+                  {formik.touched.password && formik.errors.password ? <div className="alert alert-danger" role="alert">{formik.errors.password}</div> : null}
+                   <div id="emailHelp" className="form-text">{t("message's password")}</div> 
                 </div>
                 <div className="mb-3 form-check">
-                  <input onClick={(e)=>onCheckValue(e)} type="checkbox" className="form-check-input" id="exampleCheck1" disabled={accuracy} />
+                  <input onClick={(e)=>onCheckValue(e)} type="checkbox" className="form-check-input" id="exampleCheck1" disabled={!err? false:true} />
                   <label className="form-check-label" htmlFor="exampleCheck1">{t('check me out')}</label>
                 </div>
-                <button disabled={butt}  type="submit" className="btn btn-outline-primary w-100" onClick={e=>handleSubmit(e)}>{t('register')}</button>
+                <button disabled={butt}  type="submit" className="btn btn-outline-primary w-100" onClick={formik.handleSubmit}>{t('register')}</button>
               </form>
 
 
-
+                <ToastContainer
+                 style={{ width: "500px" }}
+              />
 
             </div>
       
